e67c23f410e2d7fbd3748cb526a53d3dc4bbc1e4
Kenny Sum <keggex101@gmail.com>
2025-06-10 10:42:59 +0100

    update

---
+++ src/js/raijin_mouse.js
@@ -0,0 +1,39 @@
+function changeImage1()
+{
+    document.getElementById('main_image').src = '../../images/mouseAndKeyboard/Raijin_Mouse/Raijin_Mouse_Main_Image.jpg';
+}
+
+function changeImage2()
+{
+    document.getElementById('main_image').src = '../../images/mouseAndKeyboard/Raijin_Mouse/Raijin_Mouse_image2.jpg';
+}
+
+function changeImage3()
+{
+    document.getElementById('main_image').src = '../../images/mouseAndKeyboard/Raijin_Mouse/Raijin_Mouse_image3.jpg';
+}
+
+function changeImage4()
+{
+    document.getElementById('main_image').src = '../../images/mouseAndKeyboard/Raijin_Mouse/Raijin_Mouse_image4.jpg';
+}
+
+function changeImage5()
+{
+    document.getElementById('main_image').src = '../../images/mouseAndKeyboard/Raijin_Mouse/Raijin_Mouse_image5.jpg';
+}
+
+function changeImage6()
+{
+    document.getElementById('main_image').src = '../../images/mouseAndKeyboard/Raijin_Mouse/Raijin_Mouse_image6.jpg';
+}
+
+function changeImage7()
+{
+    document.getElementById('main_image').src = '../../images/mouseAndKeyboard/Raijin_Mouse/Raijin_Mouse_image7.jpg';
+}
+
+function changeImage8()
+{
+    document.getElementById('main_image').src = '../../images/mouseAndKeyboard/Raijin_Mouse/Raijin_Mouse_image8.jpg';
+}
